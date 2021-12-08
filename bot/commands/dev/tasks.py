c4e7f79852789f254ebbcfdc812d2951e3dcea9f
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-12-08 22:30:43 +0700

    Display coroutine name in task/tasks command

--- bot/commands/dev/tasks.py
+++ bot/commands/dev/tasks.py
@@ -14,7 +14,7 @@ def _display_frame(task: asyncio.Task) -> str:
         return "None"
 
     frame: FrameType = frames.pop() # oldest
-    return f"{frame.f_code.co_name} in {frame.f_code.co_filename}::{frame.f_code.co_firstlineno}"
+    return f"{frame.f_code.co_filename}::{frame.f_code.co_firstlineno}"
 
 
 @bot.command(
@@ -25,5 +25,5 @@ def _display_frame(task: asyncio.Task) -> str:
 @commands.is_owner()
 async def tasks_cmd(ctx: commands.Context):
     with open("./tasks.txt", "w", encoding="utf-8") as f:
-        f.write("\n".join(f"Task name='{task.get_name()}' done={task.done()} frame={_display_frame(task)}" for task in asyncio.all_tasks()))
+        f.write("\n".join(f"Task name='{task.get_name()}' coro={task.get_coro().__name__} done={task.done()} frame={_display_frame(task)}" for task in asyncio.all_tasks()))
     await ctx.send(file=discord.File("./tasks.txt"))
