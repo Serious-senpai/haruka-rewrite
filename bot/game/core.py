b7a937d6630f182d32739ac25e8d17f71728f440
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-12-13 16:08:47 +0700

    Added docstring

--- bot/game/core.py
+++ bot/game/core.py
@@ -16,7 +16,25 @@ LT = TypeVar("LT", bound="BaseLocation")
 
 
 class BaseWorld(Generic[LT]):
-    """Base class for creating new worlds"""
+    """Base class for creating new worlds.
+
+    All worlds must inherit from this class. Please note that
+    world objects are represented by the classes themselves,
+    not by their instances.
+
+    Attributes
+    -----
+    name: :class:`str`
+        The world's name
+    description: :class:`str`
+        The world's description
+    id: :class:`int`
+        The world's ID
+    locations: List[:class:`BaseLocation`]
+        The locations that this world has
+    ptypes: List[:class:`BasePlayer`]
+        List of occupations that a player can have in this world
+    """
 
     name: str
     description: str
@@ -27,12 +45,36 @@ class BaseWorld(Generic[LT]):
     @classmethod
     @functools.cache
     def from_id(cls: Type[WT], id: int) -> Optional[Type[WT]]:
+        """Get a world from its ID
+
+        Parameters
+        -----
+        id: :class:`int`
+            The world ID
+
+        Returns
+        -----
+        Optional[Type[:class:`BaseWorld`]]
+            The world with the given ID, or ``None`` if not found
+        """
         for world in cls.__subclasses__:
             if world.id == id:
                 return world
 
     @classmethod
     def get_location(cls: Type[WT], id: int) -> Optional[Type[LT]]:
+        """Get a location that belongs to this world from a given ID
+
+        Parameters
+        -----
+        id: :class:`int`
+            The location ID
+
+        Returns
+        -----
+        Optional[Type[:class:`BaseLocation`]]
+            The location with the given ID, or ``None`` if not found
+        """
         try:
             return cls.locations[id]
         except IndexError:
@@ -40,7 +82,22 @@ class BaseWorld(Generic[LT]):
 
 
 class BaseLocation(Generic[WT]):
-    """Base class for world locations"""
+    """Base class for world locations
+
+    Please note that location objects are represented by the
+    classes themselves, not by their instances.
+
+    Attributes
+    -----
+    name: :class:`str`
+        The location's name
+    description: :class:`str`
+        The location's description
+    id: :class:`int`
+        The location's ID
+    coordination: :class:`Coordination`
+        The coordination of this location in its world
+    """
 
     name: str
     description: str
@@ -50,11 +107,36 @@ class BaseLocation(Generic[WT]):
 
     @classmethod
     def from_id(cls: Type[LT], world: Type[WT], id: int) -> Optional[Type[LT]]:
+        """Get a location that belongs to a world from a given ID
+
+        Parameters
+        -----
+        world: :class:`BaseWorld`
+            The world to search from
+        id: :class:`int`
+            The location ID
+
+        Returns
+        -----
+        Optional[Type[:class:`BaseLocation`]]
+            The location with the given ID in the given world, or
+            ``None`` if not found
+        """
         return world.get_location(id)
 
 
 class BaseCreature(Battleable, Generic[LT]):
-    """Base class for creatures appearing in worlds"""
+    """Base class for creatures appearing in worlds
+
+    Attributes
+    -----
+    name: :class:`str`
+        The creature's name
+    description: :class:`str`
+        The creature's description
+    location: :class:`BaseLocation`
+        The location where this creature can be found
+    """
 
     name: str
     description: str
--- bot/game/player.py
+++ bot/game/player.py
@@ -16,7 +16,34 @@ IT = TypeVar("IT", bound="BaseItem")
 
 @dataclasses.dataclass(init=True, repr=True, order=False, frozen=False)
 class BasePlayer(Battleable, Generic[LT, WT]):
-    """Base class for players from different worlds"""
+    """Base class for players from different worlds
+
+    Each player is represented with an instance of a subclass of this class, and
+    it is up to the developer to choose whether to cache the player objects or not.
+
+    Attributes
+    -----
+    name: :class:`str`
+        The player's name, which is the same as Discord name
+    id: :class:`int`
+        The player's ID, which is the same as Discord ID
+    description: :class:`str`
+        The player's description
+    world: :class:`BaseWorld`
+        The world the player is currently in
+    location: :class:`BaseLocation`
+        The location the player is currently in
+    level: :class:`int`
+        The player's level
+    xp: :class:`int`
+        The player's experience point
+    money: :class:`int`
+        The player's money
+    items: List[:class:`BaseItem`]
+        The player's items
+    hp: :class:`int`
+        The player's current health point
+    """
 
     __slots__ = (
         "name",
@@ -41,6 +68,7 @@ class BasePlayer(Battleable, Generic[LT, WT]):
     money: int
     items: List[IT]
     hp: int
+    type_id: int
 
     # Logical operations
 
@@ -52,16 +80,25 @@ class BasePlayer(Battleable, Generic[LT, WT]):
     # Save and load operations
 
     async def update(self, conn: Union[asyncpg.Connection, asyncpg.Pool]) -> None:
+        """This function is a coroutine
+
+        Save this player's data to the database.
+
+        Parameters
+        -----
+        conn: Union[:class:`asyncpg.Connection`, :class:`asyncpg.Pool`]
+            The connection or pool to perform the operation
+        """
         await conn.execute(
-            f"UPDATE rpg\
-            SET description = $1, world = $2, location = $3,\
-                type = $4, level = $5, xp = $6, money = $7,\
-                items = $8, hp = $9\
+            f"UPDATE rpg \
+            SET description = $1, world = $2, location = $3, \
+                type = $4, level = $5, xp = $6, money = $7, \
+                items = $8, hp = $9 \
             WHERE id = '{self.id}';",
             self.description,
             self.world.id,
-            self.world.locations.index(self.location.__class__),
-            self.world.ptypes.index(self.__class__),
+            self.location.id,
+            self.type_id,
             self.level,
             self.xp,
             self.money,
@@ -75,6 +112,22 @@ class BasePlayer(Battleable, Generic[LT, WT]):
         conn: Union[asyncpg.Connection, asyncpg.Pool],
         user: discord.User,
     ) -> Optional[PT]:
+        """This function is a coroutine
+
+        Get a player object from a Discord user
+
+        Parameters
+        -----
+        conn: Union[:class:`asyncpg.Connection`, :class:`asyncpg.Pool`]
+            The connection or pool to perform the operation
+        user: :class:`discord.User`
+            The Discord user
+
+        Returns
+        -----
+        Optional[:class:`BasePlayer`]
+            The retrieved player, or ``None`` if not found
+        """
         row: asyncpg.Record = await conn.fetchrow(f"SELECT * FROM rpg WHERE id = '{user.id}';")
         if not row:
             return
@@ -94,11 +147,26 @@ class BasePlayer(Battleable, Generic[LT, WT]):
             money=row["money"],
             items=[BaseItem.from_id(item_id) for item_id in row["items"]],
             hp=row["hp"],
+            type_id=row["type"],
         )
 
 
 class BaseItem(Generic[PT]):
-    """Base class for player items"""
+    """Base class for player items.
+
+    All items must subclass this class.
+
+    Attributes
+    -----
+    name: :class:`str`
+        The item's name
+    description: :class:`str`
+        The item's description
+    id: :class:`int`
+        The item's id
+    level: :class:`int`
+        The item's level
+    """
     __slots__ = (
         "level",
     )
@@ -110,11 +178,35 @@ class BaseItem(Generic[PT]):
         self.level: int = level
 
     def effect(self, user: PT, target: Optional[Battleable]) -> Any:
+        """Perform calculation for the effect when a user consumes this item.
+\
+        Subclasses must implement this.
+
+        Parameters
+        -----
+        user: :class:`BasePlayer`
+            The player who consumed this item
+        Optional[:class:`Battleable`]
+            The effect target, if this item aims at another entity
+        """
         raise NotImplementedError
 
     @classmethod
-    def from_id(cls: Type[IT], id: str) -> Optional[IT]:
-        itype_id, ilevel = [int(s) for s in id.split(".")]
+    def from_id(cls: Type[IT], ident: str) -> Optional[IT]:
+        """Construct an item from an identification string
+
+        Parameters
+        -----
+        ident: :class:`str`
+            The identification string
+
+        Returns
+        -----
+        Optional[:class:`BaseItem`]
+            The constructed item, or ``None`` is the identification string
+            points to an invalid item type ID
+        """
+        itype_id, ilevel = [int(s) for s in ident.split(".")]
         for itype in cls.__subclasses__:
             if itype.id == itype_id:
                 return itype(ilevel)
