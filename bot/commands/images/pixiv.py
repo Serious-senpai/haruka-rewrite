6dad2ea0d304f6c678d3302e0f827b8470304311
Haruka <88233311+Saratoga-CV5@users.noreply.github.com>
2022-01-18 22:29:40 +0700

    Trigger typing in pixiv text command

--- bot/commands/images/pixiv.py
+++ bot/commands/images/pixiv.py
@@ -13,15 +13,15 @@ from core import bot
 )
 @commands.cooldown(1, 2, commands.BucketType.user)
 async def _pixiv_cmd(ctx: commands.Context, *, query: str = ""):
-    parsed = await _pixiv.parse(query, session=bot.session)
-    if isinstance(parsed, _pixiv.PixivArtwork):
-        return await ctx.send(embed=await parsed.create_embed(session=bot.session))
-
-    if not parsed:
-        return await ctx.send("No matching result was found.")
-
-    embeds = []
     async with ctx.typing():
+        parsed = await _pixiv.parse(query, session=bot.session)
+        if isinstance(parsed, _pixiv.PixivArtwork):
+            return await ctx.send(embed=await parsed.create_embed(session=bot.session))
+
+        if not parsed:
+            return await ctx.send("No matching result was found.")
+
+        embeds = []
         for index, artwork in enumerate(parsed[:6]):
             embed = await artwork.create_embed(session=bot.session)
             embed.set_footer(text=f"Displaying result #{index + 1}")
