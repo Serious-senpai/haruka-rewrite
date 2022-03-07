32293d9e5619b02507d1ff1573fd5bc7ed20d7d6
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2022-03-07 18:03:32 +0700

    Made /sauce command accepts attachments

--- bot/commands/slash_cmd/sauce.py
+++ bot/commands/slash_cmd/sauce.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+import discord
 from discord import app_commands
 
 import saucenao
@@ -9,10 +12,20 @@ from core import bot
     name="sauce",
     description="Find the image source with saucenao",
 )
-@app_commands.describe(url="The image URL")
-async def _sauce_slash(interaction: Interaction, url: str):
+@app_commands.describe(
+    image="The image to get the sauce",
+    url="The image URL",
+)
+async def _sauce_slash(interaction: Interaction, image: Optional[discord.Attachment], url: Optional[str]):
+    if image is not None:
+        image_url = image.url
+    elif url is not None:
+        image_url = url
+    else:
+        return await interaction.response.send_message("Please provide an image URL or an attachment!")
+
     await interaction.response.defer()
-    results = await saucenao.SauceResult.get_sauce(url)
+    results = await saucenao.SauceResult.get_sauce(image_url)
     if not results:
         return await interaction.followup.send("Cannot find the image sauce.")
 
@@ -22,4 +35,8 @@ async def _sauce_slash(interaction: Interaction, url: str):
         icon_url=bot.user.avatar.url,
     )
     embed.set_footer(text="For all results, consider using the text command")
-    await interaction.followup.send(embed=embed)
+
+    if image is not None and url is not None:
+        await interaction.followup.send("Both `image` and `url` were provided, using only the `image` parameter", embed=embed)
+    else:
+        await interaction.followup.send(embed=embed)
