4a5d5adc83da798f588332eb339d46178c0885fe
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2022-03-10 07:33:46 +0700

    Simplify voice disconnecting logic

--- bot/events/voice_state_update.py
+++ bot/events/voice_state_update.py
@@ -2,27 +2,22 @@ import contextlib
 
 import discord
 
-from _types import Member
-from audio import MusicClient
+from _types import Guild, Member
 from core import bot
 
 
-def _is_alone_in(voice_client: MusicClient) -> bool:
-    member = voice_client.guild.me
-    if not member.voice:
+def _is_alone_in(guild: Guild) -> bool:
+    if not guild.voice_client:
         return False
 
-    return len(voice_client.channel.members) == 1
+    return len(guild.voice_client.channel.members) == 1
 
 
 @bot.event
 async def on_voice_state_update(member: Member, before: discord.VoiceState, after: discord.VoiceState):
     guild = member.guild
-    if guild.voice_client is None:
-        return
-
-    voice_client = guild.voice_client
-    if _is_alone_in(voice_client):
+    if _is_alone_in(guild):
+        voice_client = guild.voice_client
         voice_client.pause()
         with contextlib.suppress(discord.HTTPException):
             await voice_client.target.send(f"All members have left <#{voice_client.channel.id}>. Paused audio.")
