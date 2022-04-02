82986e7499805153e70c4484d4d35c0b8aee4a72
Haruka <88233311+Kitsukaze@users.noreply.github.com>
2022-04-02 13:27:17 +0700

    Updated asset link

--- bot/lib/asset.py
+++ bot/lib/asset.py
@@ -53,7 +53,7 @@ class AssetClient:
             return await self.__finalize()
 
         with utils.TimingContextManager() as measure:
-            async with self.session.get("https://www.mediafire.com/file/e6vvkrzd0y0r7zd/collection.tar/file") as response:
+            async with self.session.get("https://www.mediafire.com/file/sqom3a6g1hrik01/collection.tar/file") as response:
                 if response.status == 200:
                     html = await response.text(encoding="utf-8")
                     soup = BeautifulSoup(html, "html.parser")
