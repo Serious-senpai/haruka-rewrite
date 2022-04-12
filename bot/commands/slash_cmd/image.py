1e36e19f4e6044595e67828a350522e2dd973a0b
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2022-04-12 19:44:25 +0700

    Implement autocomplete for ``/image``

--- bot/commands/slash_cmd/image.py
+++ bot/commands/slash_cmd/image.py
@@ -1,10 +1,11 @@
-from typing import Literal
+from typing import List, Literal
 
 import discord
 from discord import app_commands
 
 from _types import Interaction
 from core import bot
+from lib import image
 
 
 async def create_image_slash_command() -> None:
@@ -13,31 +14,31 @@ async def create_image_slash_command() -> None:
     sfw_keys = sorted(bot.image.sfw.keys())
     nsfw_keys = sorted(bot.image.nsfw.keys())
 
-    sfw_choices = [app_commands.Choice(name=sfw_keys[i], value=i) for i in range(len(sfw_keys))]
-    nsfw_choices = [app_commands.Choice(name=nsfw_keys[i], value=i) for i in range(len(nsfw_keys))]
-
     class _ImageSlashCommand(app_commands.Group):
         @app_commands.command(name="sfw", description="Get a random SFW image")
         @app_commands.describe(category="The image category")
-        @app_commands.choices(category=sfw_choices[:25])  # Discord limit
-        async def _sfw_slash(self, interaction: Interaction, category: app_commands.Choice[int]):
-            await self._process_request(interaction, "sfw", category.name)
+        async def _sfw_slash(self, interaction: Interaction, category: str):
+            await self._process_request(interaction, "sfw", category)
 
         @app_commands.command(name="nsfw", description="Get a random NSFW image")
         @app_commands.describe(category="The image category")
-        @app_commands.choices(category=nsfw_choices[:25])  # Discord limit
-        async def _nsfw_slash(self, interaction: Interaction, category: app_commands.Choice[int]):
+        async def _nsfw_slash(self, interaction: Interaction, category: str):
             try:
                 if not interaction.channel.is_nsfw():
                     return await interaction.response.send_message("🔞 This command can only be invoked in a NSFW channel.")
             except AttributeError:
                 return await interaction.response.send_message("Cannot process the command in this channel!")
 
-            await self._process_request(interaction, "nsfw", category.name)
+            await self._process_request(interaction, "nsfw", category)
 
         async def _process_request(self, interaction: Interaction, mode: Literal["sfw", "nsfw"], category: str) -> None:
             await interaction.response.defer()
-            image_url = await bot.image.get(category, mode=mode)
+
+            try:
+                image_url = await bot.image.get(category, mode=mode)
+            except image.CategoryNotFound:
+                return await interaction.followup.send(f"No category named `{category}` was found.")
+
             if image_url is None:
                 return await interaction.followup.send("Cannot fetch any images from this category right now...")
 
@@ -48,6 +49,18 @@ async def create_image_slash_command() -> None:
                 icon_url=bot.user.avatar.url,
             )
             await interaction.followup.send(embed=embed)
+
+    @_ImageSlashCommand._sfw_slash.autocomplete("category")
+    async def _sfw_autocomplete(interaction: Interaction, current: str) -> List[app_commands.Choice[str]]:
+        await interaction.response.defer()
+        result = [app_commands.Choice(name=sfw_key, value=sfw_key) for sfw_key in sfw_keys if current in sfw_key]
+        return result[:25]
+
+    @_ImageSlashCommand._nsfw_slash.autocomplete("category")
+    async def _nsfw_autocomplete(interaction: Interaction, current: str) -> List[app_commands.Choice[str]]:
+        await interaction.response.defer()
+        result = [app_commands.Choice(name=nsfw_key, value=nsfw_key) for nsfw_key in nsfw_keys if current in nsfw_key]
+        return result[:25]
 
     bot.tree.add_command(_ImageSlashCommand(name="image", description="Get a random anime image"))
 
