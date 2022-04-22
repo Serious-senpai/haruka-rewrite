5ee26221b1718d2573ca75c57839c497e192028d
Haruka <57554044+Serious-senpai@users.noreply.github.com>
2022-04-22 17:09:28 +0700

    Update 8ball.py

--- bot/commands/slash_cmd/8ball.py
+++ bot/commands/slash_cmd/8ball.py
@@ -9,6 +9,6 @@ from lib import resources
     name="8ball",
     description="Ask the 8ball a question",
 )
-@app_commands.describe(question="Concentrate on your question and press `Enter`")
+@app_commands.describe(question="A yes/no question you want to ask")
 async def _8ball_slash(interaction: Interaction, question: str):
     await interaction.response.send_message(resources.get_8ball())
